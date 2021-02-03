c1ab95bd86a738d4497b85e9eeb6365bc42c2207
Hari Kolasani <harikolasani@mac.com>
2021-02-03 11:54:47 -0800

    Initial Commit

---
+++ VaccineChecker/VaccineCheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VaccineCheckerApp.swift
+//  VaccineChecker
+//
+//  Created by Hari Kolasani on 2/3/21.
+//
+
+import SwiftUI
+
+@main
+struct VaccineCheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
